48d0e739649e8fe5b6be641952a96f052434eb65
Arrcady555 <parfenov7233@gmail.com>
2021-11-01 14:47:11 +0300

    Разбор первой программы. [#159033]-4

--- src/main/java/ru/job4j/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/ru/job4j/StudentInfo.java
@@ -4,6 +4,11 @@ public class StudentInfo {
     public static void main(String[] args) {
         System.out.println("1 * 2 = 2");
         System.out.println("1 * 3 = 3");
+        System.out.println("1 * 3 = 4");
+        System.out.println("1 * 3 = 5");
+        System.out.println("1 * 3 = 6");
+        System.out.println("1 * 3 = 7");
+        System.out.println("1 * 3 = 8");
         System.out.println("1 * 9 = 9");
     }
 }
