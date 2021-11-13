4f03807342611da91512d90e4cacae4e69549254
Arrcady555 <parfenov7233@gmail.com>
2021-11-13 21:01:17 +0300

    6.0.2. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -2,11 +2,11 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
-        short[] ages = new short[10];
-        String[] surnames = new String[100500];
-        float[] prices = new float[40];
-        System.out.println("Размер массива ages: " + ages.length);
-        System.out.println("Размер массива surnames: " + surnames.length);
-        System.out.println("Размер массива prices: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Petr Arsentev";
+        names[1] = "Stas Korobeinikov";
+        names[2] = "Elena Kartashova";
+        names[3] = "Arcady Parfenov";
+        System.out.print(names[0] + "\n" + names[1] + "\n" + names[2] + "\n" + names[3]);
     }
 }
